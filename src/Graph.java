fa7fbaac797908c701809e3332103280abdb5bbb
m.ezzeddine <mazen.ezzeddine@gmail.com>
2022-11-22 17:16:58 +0100

    A dijakstra's implementation for revision

--- src/Graph.java
+++ src/Graph.java
@@ -1,40 +1,34 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Graph {
     int V;
 
     ArrayList<ArrayList<Integer>> graph;
     Graph(int nodes) {
-
         V= nodes;
         graph = new ArrayList<ArrayList<Integer>>();
-
-
         for (int i = 0; i < nodes; i++) {
             graph.add(new ArrayList<>());
         }
-
     }
 
 
     void addEdge(int v, int u) {
         graph.get(v).add(u);
         graph.get(u).add(v);
-
     }
 
 
     void printGraph()  {
         for (int i = 0; i < V; i++) {
             System.out.println( "Node " + i);
-
             for(int x : graph.get(i)) {
                 System.out.print("child " + x + " ");
             }
             System.out.println();
-
         }
     }
 
@@ -43,7 +37,6 @@ public class Graph {
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.add(start);
         visited[start]= true;
-
         while(!queue.isEmpty()) {
             int n = queue.poll();
             System.out.println("Node " + n  );
@@ -56,8 +49,30 @@ public class Graph {
 
             }
 
-
         }
+
+
+
+
+     void DFSUtil (int start) {
+        boolean[] visited = new boolean[V];
+
+       for(int node =0; node<graph.size(); node++){
+           if(!visited[node])
+               DFSRecursive(node, visited);
+        }
+    }
+
+
+    void DFSRecursive (int node, boolean[] visited) {
+        visited[node]=true;
+        System.out.println(node);
+        for(int n : graph.get(node)) {
+            if(!visited[node])
+                DFSRecursive(n, visited);
+        }
+
+    }
 
     public static void main(String[] args) {
         Graph g = new Graph(5);
@@ -69,11 +84,10 @@ public class Graph {
         g.addEdge(3,1);
         g.addEdge(2,0);
         g.printGraph();
+        System.out.println("===========");
         g.BFS(0);
-
-
-
+        System.out.println("===========");
+        g.DFSUtil(0);
     }
-
-    }
+}
 
